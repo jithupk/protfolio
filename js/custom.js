1475f770febd97167ed9055a0260c08db00f416f
Jithu PK <jithupk96@gmail.com>
2025-11-07 11:17:52 +0530

    new swction added

--- js/custom.js
+++ js/custom.js
@@ -5,3 +5,11 @@ ScrollSmoother.create({
   effects: true, // looks for data-speed and data-lag attributes on elements
   smoothTouch: 0.1, // much shorter smoothing time on touch devices (default is NO smoothing on touch devices)
 });
+
+document.querySelectorAll(".btn-53").forEach(btn => {
+  const text = btn.textContent.trim();
+  btn.innerHTML = `
+    <div class="original">${text}</div>
+    <div class="letters">${text.split("").map(l => `<span>${l}</span>`).join("")}</div>
+  `;
+});
